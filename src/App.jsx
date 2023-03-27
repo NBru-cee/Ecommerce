f702b5f396f347ad0c4752044bb3938f811cef49
Bruce <nbruce420@gmail.com>
2023-03-27 10:29:01 +0200

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-
+import Home from "./pages/home/Home";
 function App() {
     return (
         <Router>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import Medicine from "../../assets/images/Medicine.svg";
 const Home = () => {
+    window.addEventListener("scroll", () => {
+        const header = this.document.querySelector(".header");
+        header.classList.toggle("active", this.window.scrollY > 100);
+    });
+
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <header className="header">
+            <div className="scontainer flex">
+                <div className="logo">
+                    <img src={Medicine} alt="medicine" />
+                </div>
+            </div>
+        </header>
     );
 };
 
